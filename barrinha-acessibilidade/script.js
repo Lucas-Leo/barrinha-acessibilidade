a2871d815bc7efdffeeb34c2e04d465fbb25794c
Lucas-Leo <souzaalveslucasleonardo@gmail.com>
2024-09-18 18:56:06 -0300

    Add files via upload
    Atualização da barrinha

--- barrinha-acessibilidade/script.js
+++ barrinha-acessibilidade/script.js
@@ -1,31 +1,40 @@
-// Controle de Zoom
-let currentZoom = 1;
+const widget = new window.VLibras.Widget('https://vlibras.gov.br/app');
 
-document.getElementById("aumentarzoom").addEventListener("click", () => {
-    currentZoom += 0.1;
-    document.body.style.transform = `scale(${currentZoom})`;
-});
+const toggleButton = document.getElementById('toggleVlibras');
+const vlibrasIcon = document.getElementById('vlibrasIcon'); 
 
-document.getElementById("diminuirzoom").addEventListener("click", () => {
-    if (currentZoom > 0.5) {
-        currentZoom -= 0.1;
-        document.body.style.transform = `scale(${currentZoom})`;
-    }
-});
+let vlibrasActive = false;
 
-document.getElementById("resetarzoom").addEventListener("click", () => {
-    currentZoom = 1;
-    document.body.style.transform = "scale(1)";
-});
-
-// Alternância de Modo Claro/Escuro
-const themeToggleBtn = document.getElementById("alterartema");
-let darkMode = false;
-
-themeToggleBtn.addEventListener("click", () => {
-    darkMode = !darkMode;
-    document.body.classList.toggle("modoescuro", darkMode);
-    document.body.classList.toggle("modoclaro", !darkMode);
+toggleButton.addEventListener('click', () => {
+    const vlibrasElement = document.querySelector('[vw]');
     
-    themeToggleBtn.textContent = darkMode ? "☀️" : "🌙";
+    if (vlibrasActive) {
+        vlibrasElement.style.display = 'none';
+        // Alterar para imagem de ativar VLibras
+        vlibrasIcon.src = 'img/libras_pretebranco.png';
+        vlibrasIcon.alt = 'Desativar VLibras';
+    } else {
+        vlibrasElement.style.display = 'block';
+        // Alterar para imagem de desativar VLibras
+        vlibrasIcon.src = 'img/libras.png'; 
+        vlibrasIcon.alt = 'Ativar VLibras';
+    }   
+    vlibrasActive = !vlibrasActive;
+});
+
+const toggleMode = document.getElementById('toggleMode');
+
+// Evento para alternar entre modo noite e dia
+toggleMode.addEventListener('change', () => {
+    if (toggleMode.checked) {
+        // Modo noite
+        console.log("Modo Noite");
+        document.body.style.backgroundColor = "#2c2c2c"; // Fundo escuro
+        document.body.style.color = "#ffffff"; // Texto branco
+    } else {
+        // Modo dia
+        console.log("Modo Dia");
+        document.body.style.backgroundColor = "#ffffff"; // Fundo branco
+        document.body.style.color = "#000000"; // Texto preto
+    }
 });
